5bc6fdcfc043b8a07afa4544d534135538558408
Daniele Conti <fourlastor@users.noreply.github.com>
2022-04-16 14:36:36 +0100

    Add android

---
+++ android/src/main/java/io/github/fourlastor/keys/DatabaseList.kt
@@ -0,0 +1,9 @@
+package io.github.fourlastor.keys
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DatabaseList() {
+    Text(text = "database list")
+}
---
+++ android/src/main/java/io/github/fourlastor/keys/MainActivity.kt
@@ -0,0 +1,49 @@
+package io.github.fourlastor.keys
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import io.github.fourlastor.keys.ui.theme.KeysTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    KeysTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "database") {
+                navigation("list", "database") {
+                    composable("list") {
+                        DatabaseList()
+                    }
+                    navigation("list", "keys") {
+                        composable("list") {
+                            KeyList()
+                        }
+                        composable("detail") {
+                            KeyDetail()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
